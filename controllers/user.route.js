cd68fbe423e2180722e8afc719309d1a2a7d3253
Altinose <73234051+Altinose@users.noreply.github.com>
2021-12-13 00:04:42 +0100

    menu ejs

--- controllers/user.route.js
+++ controllers/user.route.js
@@ -16,8 +16,18 @@ router.get("/email", emaildisplay);
 
 router.get("/event", eventdisplay);
 
+
+router.get("/menu", menudisplay);
+
+
 router.get("/welcome", showwelcome);
 router.get("/", redir);
+
+
+
+function menudisplay(request, response) {
+    response.render("menu");
+}
 
 function eventdisplay(request, response) {
     response.render("event");
