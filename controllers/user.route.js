c6df25a8731531c31a2fbff1481de7d810893dfd
Altinose <73234051+Altinose@users.noreply.github.com>
2021-12-13 14:05:54 +0100

    Login working2

--- controllers/user.route.js
+++ controllers/user.route.js
@@ -1,16 +1,21 @@
-const { response } = require('express');
 const express = require('express');
 const router = express.Router();
+
 const gradeRepo = require('../utils/user.repository');
+const userauth = require('../utils/users.auth');
 
 
 
 router.get("/grades/:student_id/:filter", grades);
 router.get("/grades/:student_id", moovetograde);
 
-
+//router.get("/encryption", encryption);
 router.get("/connexion", connectiondisplay);
 
+router.post("/connexion/check", checkconnexion);
+
+router.get("/connexion/admin", userauth.checkAuthentication("admin"), redir);
+router.get("/connexion/user", userauth.checkAuthentication("user"), redir);
 
 router.get("/email", emaildisplay);
 
@@ -50,8 +55,38 @@ function showwelcome(request, response) {
 function redir(request, responce) {
     responce.redirect("welcome");
 }
+/*
+function encryption(request, response) {
+    for (var i=0;i<=77; i++){
+    gradeRepo.encryptionmdp(i);
+    
+    }
+    response.redirect("/");
+
+}
+*/
 
 
+async function checkconnexion(request, response) {
+    console.log(request.body.username, request.body.passwd);
+
+    areValid = await gradeRepo.areValidCredentials(request.body.username, request.body.passwd);
+    console.log(areValid);
+    if (areValid) {
+        user = await gradeRepo.getOneUser(request.body.username);
+        console.log(user);
+        if (user.login_level === "admin") {
+            return response.redirect("/connexion/admin");
+        } else {
+            console.log("bb");
+            return response.redirect("/connexion/user");
+        }
+
+    } else {
+        response.send("Invalid credentials provided");
+    }
+
+}
 
 
 
--- server.js
+++ server.js
@@ -5,29 +5,32 @@ dotenv.config();
 
 app.listen(process.env.WEB_PORT,
 
-    function () { console.log("Listenin on" + process.env.WEB_PORT); }
+    function () { console.log("Listening at " + process.env.WEB_PORT); }
 );
-/*
-app.get('/welcome', (req, res) => {
-    res.render("welcome")
-});
 
-app.get('/welcome/menu', (req, res) => {
-    res.render("menu")
-});
-*/
+const session = require("express-session");
+app.use(session({
+    secret: "SecretRandomKey123QWE",
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    resave: false
+}));
+
+// add after SESSION
+const auth = require("./utils/users.auth");
+auth.initialization(app);
+
+// add first
+
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 
 
-// * app.use(callback1, callback2);
-// * app.use(routeBase, callback);
 
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
-// * app.use("/cars", require("./controllers/cars.route"));
 app.use("/static", express.static(__dirname + '/static'));
 app.use("/", require("./controllers/user.route"));
